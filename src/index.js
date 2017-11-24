7697fa9cf8744d59596a684ced763c28bd3989be
candle <ted@wicrep.com>
2017-11-24 15:30:50 +0900

    Create basic main script and babelrc

---
+++ src/index.js
@@ -0,0 +1,14 @@
+module.exports = babel => {
+  var t = babel.types;
+
+  return {
+    name: "s2s-axios-api-manager",
+    visitor: {
+      Program: {
+        enter(path, state) {
+
+        }
+      }
+    }
+  }
+}
